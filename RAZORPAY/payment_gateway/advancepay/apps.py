4bd790482bb06a39d92377dc3114ffcd699285e6
Data-Mind 2.0 <112772762+datamind321@users.noreply.github.com>
2024-01-26 15:07:43 +0530

    Delete RAZORPAY/payment_gateway/advancepay directory

--- RAZORPAY/payment_gateway/advancepay/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdvancepayConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'advancepay'
--- RAZORPAY/payment_gateway/advancepay/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-from .views import create_payment,payment_success,payment_failed
-
-urlpatterns = [
-    path('create-payment/',create_payment,name='create_payment'),
-    path('payment-success/',payment_success,name='payment_success'),
-    path('payment-failed/',payment_failed,name='payment_failed'),
-]
